ce3f302d8a633165ead68ace1b86c25fa7990067
黃敏睿 <rayhuang@linkchain.tw>
2023-01-06 08:18:04 +0800

    WIP

--- TestProject/UnitTestLandNoTool.cs
+++ TestProject/UnitTestLandNoTool.cs
@@ -1,3 +1,4 @@
+using FluentAssertions.Execution;
 using LandStandardLibrary;
 
 namespace LandNoToolTests
@@ -13,10 +14,15 @@ namespace LandNoToolTests
             string expectedResult = "4567";
 
             // Act 
-            var actualResult = new LandNoTool(testLandno).Trim().Build().FirstOrDefault();
+            var actualResult = new LandNoTool(testLandno).Trim().Build();
 
-            // Assert            
-            actualResult.Should().BeEquivalentTo(expectedResult);
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.LandNo.Should().BeEquivalentTo(expectedResult)
+                );
+            }                
         }
 
         [Fact]
@@ -24,13 +30,16 @@ namespace LandNoToolTests
         {
             // Arrange 
             var testLandno = new List<string> { " 4 5 6 7 ", "34", "5 67", "ab c " };
-            string expectedResult = "4567";
+            var expectedResult = new List<string> { "4567", "34", "567", "abc" };
 
             // Act 
             var actualResult = new LandNoTool(testLandno).Trim().Build();
 
-            // Assert            
-            actualResult.Should().BeEquivalentTo(expectedResult);
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Select(a => a.LandNo).Should().BeEquivalentTo(expectedResult);
+            }            
         }
         #endregion TestTrim
 
@@ -43,12 +52,209 @@ namespace LandNoToolTests
             string expectedResult = "1234";
 
             // Act 
-            var actualResult = new LandNoTool(testLandno).FullToHalf().Build().FirstOrDefault();            
+            var actualResult = new LandNoTool(testLandno).FullToHalf().Build();
 
-            // Assert            
-            actualResult.Should().BeEquivalentTo(expectedResult);
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.LandNo.Should().BeEquivalentTo(expectedResult)
+                );
+            }                
         }
         #endregion TestFullToHalf
+
+        #region TestVerify
+        [Fact]
+        public void TestVerify_1()
+        {
+            // Arrange 
+            string testLandno = "(河川地地號)";            
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeFalse()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("自定義地號")
+                );
+            }
+        }
+
+        [Fact]
+        public void TestVerify_2()
+        {
+            // Arrange 
+            string testLandno = "5-1(內)";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeFalse()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("非標準地號")
+                );
+            }
+        }
+
+
+        [Fact]
+        public void TestVerify_3()
+        {
+            // Arrange 
+            string testLandno = "5-1-3";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeFalse()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("非標準地號")
+                );
+            }
+        }
+
+        [Fact]
+        public void TestVerify_4()
+        {
+            // Arrange 
+            string testLandno = "五-二";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeFalse()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("非標準地號")
+                );
+            }
+        }
+
+        [Fact]
+        public void TestVerify_5()
+        {
+            // Arrange 
+            string testLandno = "5";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeTrue()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("")
+                );
+            }
+        }
+
+        [Fact]
+        public void TestVerify_6()
+        {
+            // Arrange 
+            string testLandno = "04-002";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeTrue()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("")
+                );
+            }
+        }
+
+        [Fact]
+        public void TestVerify_7()
+        {
+            // Arrange 
+            string testLandno = "12345-2";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeFalse()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("非標準地號")
+                );
+            }
+        }
+
+        [Fact]
+        public void TestVerify_8()
+        {
+            // Arrange 
+            string testLandno = "5-34860";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeFalse()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("非標準地號")
+                );
+            }
+        }
+
+        [Fact]
+        public void TestVerify_9()
+        {
+            // Arrange 
+            string testLandno = "123456789";
+
+            // Act 
+            var actualResult = new LandNoTool(testLandno).Verify().Build();
+
+            // Assert
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.IsNormalized.Should().BeFalse()
+                );
+                actualResult.Should().AllSatisfy(x =>
+                    x.ErrorMsg.Should().BeEquivalentTo("非標準地號")
+                );
+            }
+        }
+        #endregion TestVerify
 
         #region TestPadZero
         [Fact]
@@ -59,10 +265,15 @@ namespace LandNoToolTests
             string expectedResult = "0034-0078";
 
             // Act 
-            var actualResult = new LandNoTool(testLandno).PadZero().Build().FirstOrDefault();
+            var actualResult = new LandNoTool(testLandno).Verify().PadZero().Build();
 
             // Assert
-            actualResult.Should().BeEquivalentTo(expectedResult);
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.LandNo.Should().BeEquivalentTo(expectedResult)
+                );
+            }
         }
 
         [Fact]
@@ -73,10 +284,15 @@ namespace LandNoToolTests
             string expectedResult = "0034-0000";
 
             // Act 
-            var actualResult = new LandNoTool(testLandno).PadZero().Build().FirstOrDefault();
+            var actualResult = new LandNoTool(testLandno).Verify().PadZero().Build();
 
             // Assert
-            actualResult.Should().BeEquivalentTo(expectedResult);
+            using (new AssertionScope())
+            {
+                actualResult.Should().AllSatisfy(x =>
+                    x.LandNo.Should().BeEquivalentTo(expectedResult)
+                );
+            }
         }
         #endregion TestPadZero
 
@@ -86,13 +302,16 @@ namespace LandNoToolTests
         {
             // Arrange 
             var testLandno = new List<string> { " 4 5 6 7 ", "34", "5 67", "ab c " };
-            var expectedResult = new LandNoTool(testLandno).Trim().FullToHalf().PadZero().Build();
+            var expectedResult = new LandNoTool(testLandno).Trim().FullToHalf().Verify().PadZero().Build();
 
             // Act 
             var actualResult = new LandNoTool(testLandno).Normalize().Build();
-            
+
             // Assert            
-            actualResult.Should().BeEquivalentTo(expectedResult);
+            using (new AssertionScope())
+            {
+                actualResult.Should().BeEquivalentTo(expectedResult);
+            }
         }
         #endregion TestNormalize
     }
